b5a2c3d889ccbe75350d556dfdebac312e0042cf
majdkh97 <majd.motaz@yahoo.com>
2021-04-04 14:19:26 +0300

    labtime

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+'use strict';
+let userName = prompt('what\'s your name?');
+console.log(typeof userName);
+
+let userAge = prompt("what\'s your age?");
+console.log(typeof userAge);
+
+let memeQue = prompt('do you like memes?');
+console.log(typeof memeQue);
+
+let favMeme = prompt('what\'s your favorite meme');
+console.log(typeof favMeme);
+
+alert('Your name is ' + userName + ' and your age is ' + userAge + ' do you like memes ' + memeQue + ' Your Favorite meme is ' + favMeme);
